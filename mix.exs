97a4da6eb41211b6ff60c04fa3e563c49a6dc587
Vicente Santos <vicentedpsantos@gmail.com>
2026-01-29 11:56:59 -0300

    Fix CI

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule AnalyticsElixir.Mixfile do
       app: :segment,
       deps: deps(),
       description: "analytics_elixir",
-      elixir: "~> 1.12",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       name: "analytics_elixir",
       package: package(),
